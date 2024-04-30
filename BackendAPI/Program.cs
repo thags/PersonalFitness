1b24045d8dab27a8e4670afbbff299dcd3deb6ff
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2024-04-29 19:03:55 -0500

    add cors to api

--- BackendAPI/Program.cs
+++ BackendAPI/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddJsonOptions(options =>
     {
@@ -38,7 +39,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(
+        options => options.WithOrigins("http://localhost:5126").AllowAnyMethod()
+    );
 app.UseHttpsRedirection();
 
 app.MapControllers();
