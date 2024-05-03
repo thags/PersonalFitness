ea99b0eecdda1c6de1b1ab8ed5e0ca089cb79b8c
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2024-05-02 21:52:28 -0500

    Add toggling theme light/dark

--- FrontEndReact2/fitness/src/App.tsx
+++ FrontEndReact2/fitness/src/App.tsx
@@ -2,16 +2,19 @@ import "./App.css";
 import CreateExercise from "./components/CreateExercise";
 import ListExercises from "./components/ListExercises";
 import ListWorkouts from "./components/ListWorkouts";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <ModeToggle />
       <ListExercises
         onSelectItem={() => console.log("you selected something")}
       ></ListExercises>
       <CreateExercise></CreateExercise>
       <ListWorkouts />
-    </>
+    </ThemeProvider>
   );
 }
 
